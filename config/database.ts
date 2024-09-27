ec3da4d5afc9d47fb7ef38a022722e61f44f166a
khanh-ptit <168727980+khanh-ptit@users.noreply.github.com>
2024-09-27 10:11:29 +0700

    Kết nối DB

---
+++ config/database.ts
@@ -0,0 +1,10 @@
+import mongoose from "mongoose"
+
+export const connect = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URL)
+        console.log("Successfully connected")
+    } catch (error) {
+        console.log("Error when connected")
+    }
+}
--- index.ts
+++ index.ts
@@ -1,7 +1,12 @@
 import express, {Express, Request, Response} from "express"
+import * as database from "./config/database"
+import dotenv from "dotenv"
+
+dotenv.config()
+database.connect()
 
 const app: Express = express()
-const port: number = 3000
+const port: number = parseInt(process.env.PORT.toString())
 
 app.set("views", "./views")
 app.set("view engine", "pug")
