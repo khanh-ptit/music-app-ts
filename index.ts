9e41f4cad9e511ece68ea435772e492b97e16998
khanh-ptit <168727980+khanh-ptit@users.noreply.github.com>
2024-09-27 10:20:11 +0700

    Lấy ra chủ đề bài hát

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import express, {Express, Request, Response} from "express"
 import * as database from "./config/database"
 import dotenv from "dotenv"
+import Topic from "./models/topic.model"
 
 dotenv.config()
 database.connect()
@@ -11,7 +12,11 @@ const port: number = parseInt(process.env.PORT.toString())
 app.set("views", "./views")
 app.set("view engine", "pug")
 
-app.get("/topics", (req: Request, res: Response) => {
+app.get("/topics", async (req: Request, res: Response) => {
+    const topics = await Topic.find({
+        deleted: false
+    })
+    console.log(topics)
     res.render("client/pages/topics/index.pug")
 })
 
