3e437a8049052f21fc0139c6eba0b9bd7091bba9
khanh-ptit <168727980+khanh-ptit@users.noreply.github.com>
2024-09-27 17:29:10 +0700

    Làm trang danh sách bài hát

---
+++ controllers/client/song.controller.ts
@@ -0,0 +1,32 @@
+import { Request, Response } from "express"
+import Topic from "../../models/topic.model"
+import Song from "../../models/song.model"
+import Singer from "../../models/singer.model"
+
+// [GET] /songs/:slugTopic
+export const list = async (req: Request, res: Response): Promise<void> => {
+    // console.log(req.params.slugTopic)
+    const topic = await Topic.findOne({
+        slug: req.params.slugTopic,
+        status: "active",
+        deleted: false
+    })
+    const topicId = topic.id
+    const songs = await Song.find({
+        topicId: topicId,
+        status: "active",
+        deleted: false
+    }).select("avatar title slug singerId like")
+    
+    for (const item of songs) {
+        const singerInfo = await Singer.findOne({
+            _id: item.singerId
+        })
+        item["singerInfo"] = singerInfo
+    }
+
+    res.render("client/pages/songs/list", {
+        pageTitle: topic.title,
+        songs: songs
+    })
+}
--- routes/client/index.route.ts
+++ routes/client/index.route.ts
@@ -1,8 +1,10 @@
 import { Express } from "express"
 import { topicRoutes } from "./topic.route"
+import { songRoutes } from "./song.route"
 
 const routeClient = (app: Express): void => {
     app.use("/topics", topicRoutes)
+    app.use("/songs", songRoutes)
 }
 
 export default routeClient
