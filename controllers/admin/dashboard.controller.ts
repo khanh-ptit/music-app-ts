ef5667c2d1f33c1275a9f1c25010061c77cb6c5b
khanh-ptit <168727980+khanh-ptit@users.noreply.github.com>
2024-10-06 16:27:57 +0700

    Làm trang dashboard

--- controllers/admin/dashboard.controller.ts
+++ controllers/admin/dashboard.controller.ts
@@ -1,8 +1,111 @@
 import { Request, Response } from "express";
+import Topic from "../../models/topic.model";
+import Song from "../../models/song.model";
+import Account from "../../models/account.model";
+import User from "../../models/user.model";
+import Singer from "../../models/singer.model";
+import Role from "../../models/role.model";
 
 // [GET] /admin/dashboard
-export const index = (req: Request, res: Response) => {
+export const index = async (req: Request, res: Response) => {
+    const statistic = {
+        topic: {
+            total: 0,
+            active: 0,
+            inactive: 0
+        }, 
+        song: {
+            total: 0,
+            active: 0,
+            inactive: 0
+        }, 
+        singer: {
+            total: 0,
+            active: 0,
+            inactive: 0
+        }, 
+        role: {
+            total: 0
+        }, 
+        clientAccount: {
+            total: 0,
+            active: 0,
+            inactive: 0,
+            initial: 0
+        }, 
+        adminAccount: {
+            total: 0,
+            active: 0,
+            inactive: 0
+        }, 
+    }
+
+    statistic.topic.total = await Topic.countDocuments({
+        deleted: false
+    })
+    statistic.topic.active = await Topic.countDocuments({
+        status: "active",
+        deleted: false
+    })
+    statistic.topic.inactive = await Topic.countDocuments({
+        status: "inactive",
+        deleted: false
+    })
+
+    statistic.song.total = await Song.countDocuments({
+        deleted: false
+    })
+    statistic.song.active = await Song.countDocuments({
+        status: "active",
+        deleted: false
+    })
+    statistic.song.inactive = await Song.countDocuments({
+        status: "inactive",
+        deleted: false
+    })
+    statistic.adminAccount.total = await Account.countDocuments({
+        deleted: false
+    })
+    statistic.adminAccount.active = await Account.countDocuments({
+        status: "active",
+        deleted: false
+    })
+    statistic.adminAccount.inactive = await Account.countDocuments({
+        status: "inactive",
+        deleted: false
+    })
+    statistic.clientAccount.total = await User.countDocuments({
+        deleted: false
+    })
+    statistic.clientAccount.active = await User.countDocuments({
+        status: "active",
+        deleted: false
+    })
+    statistic.clientAccount.inactive = await User.countDocuments({
+        status: "inactive",
+        deleted: false
+    })
+    statistic.clientAccount.initial = await User.countDocuments({
+        status: "initial",
+        deleted: false
+    })
+    statistic.singer.total = await Singer.countDocuments({
+        deleted: false
+    })
+    statistic.singer.active = await Singer.countDocuments({
+        status: "active",
+        deleted: false
+    })
+    statistic.singer.inactive = await Singer.countDocuments({
+        status: "inactive",
+        deleted: false
+    })
+    statistic.role.total = await Role.countDocuments({
+        deleted: false
+    })
+
     res.render("admin/pages/dashboard/index", {
-        pageTitle: "Trang tổng quan"
+        pageTitle: "Trang tổng quan",
+        statistic: statistic
     })
 }
--- dist/controllers/admin/dashboard.controller.js
+++ dist/controllers/admin/dashboard.controller.js
@@ -1,9 +1,121 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.index = void 0;
-const index = (req, res) => {
-    res.render("admin/pages/dashboard/index", {
-        pageTitle: "Trang tổng quan"
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.index = void 0;
+const topic_model_1 = __importDefault(require("../../models/topic.model"));
+const song_model_1 = __importDefault(require("../../models/song.model"));
+const account_model_1 = __importDefault(require("../../models/account.model"));
+const user_model_1 = __importDefault(require("../../models/user.model"));
+const singer_model_1 = __importDefault(require("../../models/singer.model"));
+const role_model_1 = __importDefault(require("../../models/role.model"));
+const index = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const statistic = {
+        topic: {
+            total: 0,
+            active: 0,
+            inactive: 0
+        },
+        song: {
+            total: 0,
+            active: 0,
+            inactive: 0
+        },
+        singer: {
+            total: 0,
+            active: 0,
+            inactive: 0
+        },
+        role: {
+            total: 0
+        },
+        clientAccount: {
+            total: 0,
+            active: 0,
+            inactive: 0,
+            initial: 0
+        },
+        adminAccount: {
+            total: 0,
+            active: 0,
+            inactive: 0
+        },
+    };
+    statistic.topic.total = yield topic_model_1.default.countDocuments({
+        deleted: false
+    });
+    statistic.topic.active = yield topic_model_1.default.countDocuments({
+        status: "active",
+        deleted: false
+    });
+    statistic.topic.inactive = yield topic_model_1.default.countDocuments({
+        status: "inactive",
+        deleted: false
+    });
+    statistic.song.total = yield song_model_1.default.countDocuments({
+        deleted: false
+    });
+    statistic.song.active = yield song_model_1.default.countDocuments({
+        status: "active",
+        deleted: false
+    });
+    statistic.song.inactive = yield song_model_1.default.countDocuments({
+        status: "inactive",
+        deleted: false
+    });
+    statistic.adminAccount.total = yield account_model_1.default.countDocuments({
+        deleted: false
+    });
+    statistic.adminAccount.active = yield account_model_1.default.countDocuments({
+        status: "active",
+        deleted: false
+    });
+    statistic.adminAccount.inactive = yield account_model_1.default.countDocuments({
+        status: "inactive",
+        deleted: false
+    });
+    statistic.clientAccount.total = yield user_model_1.default.countDocuments({
+        deleted: false
+    });
+    statistic.clientAccount.active = yield user_model_1.default.countDocuments({
+        status: "active",
+        deleted: false
+    });
+    statistic.clientAccount.inactive = yield user_model_1.default.countDocuments({
+        status: "inactive",
+        deleted: false
+    });
+    statistic.clientAccount.initial = yield user_model_1.default.countDocuments({
+        status: "initial",
+        deleted: false
+    });
+    statistic.singer.total = yield singer_model_1.default.countDocuments({
+        deleted: false
+    });
+    statistic.singer.active = yield singer_model_1.default.countDocuments({
+        status: "active",
+        deleted: false
+    });
+    statistic.singer.inactive = yield singer_model_1.default.countDocuments({
+        status: "inactive",
+        deleted: false
+    });
+    statistic.role.total = yield role_model_1.default.countDocuments({
+        deleted: false
+    });
+    res.render("admin/pages/dashboard/index", {
+        pageTitle: "Trang tổng quan",
+        statistic: statistic
+    });
+});
 exports.index = index;
