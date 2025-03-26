f5a9bcdc75b2492f0505b8418e11593c0bed358a
khanh-ptit <168727980+khanh-ptit@users.noreply.github.com>
2025-03-26 18:42:44 +0700

    Update textflow api to send SMS OTP

--- dist/helpers/sendSms.js
+++ dist/helpers/sendSms.js
@@ -7,33 +7,33 @@ exports.sendSMS = void 0;
 const https_1 = __importDefault(require("https"));
 const ACCESS_TOKEN = process.env.SPEEDSMS_API_KEY;
 const sendSMS = (phones, content, type) => {
-    const url = 'api.speedsms.vn';
+    const url = "api.speedsms.vn";
     const params = JSON.stringify({
         to: phones,
         content: content,
-        sms_type: type
+        sms_type: type,
     });
-    const buf = Buffer.from(ACCESS_TOKEN + ':x');
-    const auth = "Basic " + buf.toString('base64');
+    const buf = Buffer.from(ACCESS_TOKEN + ":x");
+    const auth = "Basic " + buf.toString("base64");
     const options = {
         hostname: url,
         port: 443,
-        path: '/index.php/sms/send',
-        method: 'POST',
+        path: "/index.php/sms/send",
+        method: "POST",
         headers: {
-            'Content-Type': 'application/json',
-            'Authorization': auth
-        }
+            "Content-Type": "application/json",
+            Authorization: auth,
+        },
     };
     const req = https_1.default.request(options, (res) => {
-        let body = '';
-        res.setEncoding('utf8');
-        res.on('data', (d) => {
+        let body = "";
+        res.setEncoding("utf8");
+        res.on("data", (d) => {
             body += d;
         });
-        res.on('end', () => {
+        res.on("end", () => {
             const json = JSON.parse(body);
-            if (json.status === 'success') {
+            if (json.status === "success") {
                 console.log("Send SMS success");
             }
             else {
@@ -41,7 +41,7 @@ const sendSMS = (phones, content, type) => {
             }
         });
     });
-    req.on('error', (e) => {
+    req.on("error", (e) => {
         console.log("Send SMS failed due to error: " + e.message);
     });
     req.write(params);
